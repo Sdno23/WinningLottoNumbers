7946a37649a83ee192b4690fe6cdfde885de462d
Sdno23 <103979026+Sdno23@users.noreply.github.com>
2023-11-06 00:18:20 -1000

    Update Results.java

--- Results.java
+++ Results.java
@@ -1,5 +1,5 @@
 /*
- * @author 3156908
+ * @author XXXXX08
  *
  * @version 1.0 07/14/2020
  *
@@ -9,11 +9,11 @@
  *
  * Lecturer's Name: C. Charters
  */
-package main;
+package winninglottonumbers;
 
 /**
  *
- * @author 3156908
+ * @author XXXXX08
  */
 public class Results {     
 
